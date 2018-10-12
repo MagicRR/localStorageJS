1e11922f6e4affbf6ad8cdbf29a73748e8c70a43
Allan77230 <predaboss@gmail.com>
2018-10-12 09:54:01 +0200

    add revision

--- js/script.js
+++ js/script.js
@@ -4,22 +4,18 @@ $(document).ready(function(){
 
     };
 
-    if(typeof localStorage!='undefined')
-    {
-        if('message' in localStorage)
-        {
+    if(typeof localStorage != 'undefined'){
+        if('message' in localStorage){
             // alert("Message récupéré");
             document.getElementById('message').value = localStorage.getItem('message');
         }
 
         var nbvisites = localStorage.getItem('visites');
 
-        if(nbvisites!=null)
-        {
+        if( nbvisites != null ){
           nbvisites = parseInt(nbvisites);
         }
-        else
-        {
+        else{
           nbvisites = 1;
         }
 
@@ -29,6 +25,26 @@ $(document).ready(function(){
 
         // document.getElementById('visites').innerHTML = nbvisites;
         console.log(localStorage.visites);
+
+        $('#save').click(function(){
+
+            var revisions;
+
+            if( null != localStorage.getItem( JSON.stringify('revisions')) ){
+                revisions = localStorage.getItem( JSON.stringify('revisions'));
+                revisions.revisions++;
+
+            }else{
+                revisions = {
+                    revisions : 1
+                };
+            }
+
+            console.log(revisions.revisions);
+
+
+            localStorage.setItem('revisions',JSON.stringify(revisions));
+        });
 
     }
     else

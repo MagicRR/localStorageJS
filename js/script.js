168c2732d342a46822b9462840290e78a90f98a8
MagicRR <r.richard.94@orange.fr>
2018-10-12 09:18:23 +0200

    Add JS.JS et voilà

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+    window.onbeforeunload = function (e) {
+    };
+
+    if(typeof localStorage!='undefined')
+    {
+        if('message' in localStorage)
+        {
+            // alert("Message récupéré");
+            document.getElementById('message').value = localStorage.getItem('message');
+        }
+
+        var nbvisites = localStorage.getItem('visites');
+
+        if(nbvisites!=null)
+        {
+          nbvisites = parseInt(nbvisites);
+        }
+        else
+        {
+          nbvisites = 1;
+        }
+
+        nbvisites++;
+
+        localStorage.setItem('visites', nbvisites);
+
+        // document.getElementById('visites').innerHTML = nbvisites;
+        console.log(localStorage.visites);
+
+    }
+    else
+    {
+      alert("localStorage n'est pas supporté");
+    }
+
+});

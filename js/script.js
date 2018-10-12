8c43f55f1c1e2260a0d46a540cdc7214a846ca8c
Allan77230 <predaboss@gmail.com>
2018-10-12 16:05:47 +0200

    ajout date

--- js/script.js
+++ js/script.js
@@ -11,33 +11,47 @@ $(document).ready(function(){
 
         if( revisions != null ){
             for( var i = 0; i < revisions.length; i++){
-                $('#select_revision').append(new Option( i+1, revisions[i] ));
+                $('#select_revision').append(new Option( revisions[i].date + ' - ' + (i+1), revisions[i] ));
             }
         }
 
         $('#save').click(function(){
 
+            //Récupère l'objet révisions du localstorage
             var revisions = JSON.parse(localStorage.getItem('revisions'));
 
-            if( revisions != null ){
-                revisions.push({message: localStorage.getItem('message')});
-                console.log(revisions);
+            //Définis la date courante
+            var date = new Date();
+            var twoDigitMonth = ( date.getMonth() > 8)? (date.getMonth()+1) : '0' + (date.getMonth()+1);
+            var min = ( date.getMinutes() > 9)? (date.getMinutes()+1) : '0' + (date.getMinutes()+1);
+            var currentDate = date.getHours() + ':' + min + ' - ' + date.getDate() + "/" + twoDigitMonth + "/" + date.getFullYear();
 
+            //Si il n'existe pas d'objet révisions dans le localStorage alors on en initialise un,
+            //sinon on rajoute la nouvelle sauvegarde à la suite de l'existant
+            if( revisions != null ){
+                revisions.push({message: localStorage.getItem('message'), date: currentDate});
             }else{
-                revisions = [{message: localStorage.getItem('message') }];
+                revisions = [{message: localStorage.getItem('message'), date: currentDate}];
             }
 
+            //On met à jour le localStorage de revisions
             localStorage.setItem('revisions',JSON.stringify(revisions));
 
-            $('#select_revision').append(new Option( revisions.length, revisions.length ));
+            //On met à jours le <select> afin d'avoir la liste des révisions actualisées
+            $('#select_revision').append(new Option( revisions[revisions.length-1].date + ' - ' + (revisions.length), revisions.length-1 ));
 
         });
 
         $('#load').click(function(){
             var revisions = JSON.parse(localStorage.getItem('revisions'));
             var id_revision = $('#select_revision option:selected').text();
-            console.log(revisions);
-            console.log(revisions[id_revision-1].message);
+
+            //Si on a plus de 22 caractères alors on garde les 2 derniers carac sinon si on est à plus on prend les 3 derniers
+            if($('#select_revision option:selected').text().length <= 22 ){
+                id_revision = $('#select_revision option:selected').text().substr($('#select_revision option:selected').text().length-2, $('#select_revision option:selected').text().length);
+            }else if ($('#select_revision option:selected').text().length > 22){
+                id_revision = $('#select_revision option:selected').text().substr($('#select_revision option:selected').text().length-3, $('#select_revision option:selected').text().length);
+            }
             $('#message').val(revisions[id_revision-1].message);
         });
 
